8bdffff84f023891b4fd3733025c27094838a043
RayAntariksha <antariksharay20@gmail.com>
2026-03-25 21:36:33 +0530

    Fixed colorscheme and added transparent background. I would probably need to re-write the config after the 0.12 release. I am very excited to see the 0.12 release of neovim. But still I am not sure how to react as I have seen many criticism online for it being too "bloated". I don't want to share my opinion right now- We will see for  ourself how good it is going to be after 2 days. 25/03/2026 ~ RayAntariksha

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ vim.o.relativenumber = true
 vim.o.cursorline = true
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
+vim.o.scrolloff = 8
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.textwidth = 80
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
 return{
-    "AbdelrahmanDwedar/awesome-nvim-colorschemes",
-    config = function ()
-        
-    end
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "storm", -- Choose your preferred style: "storm", "moon", "night", or "day"
+    transparent = true, -- Enable general transparency
+    styles = {
+      sidebars = "transparent", -- Make sidebars (like nvim-tree) transparent
+      floats = "transparent",  -- Make floating windows transparent
+    },
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd.colorscheme("tokyonight")
+  end,
 }
+
